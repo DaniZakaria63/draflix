2bc82376a3700c577640b8ad1f79adf620a77a26
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-08-23 19:53:56 +0700

    feat: app configuration

--- app/src/main/java/dev/daniza/draflix/DraflixActivity.kt
+++ app/src/main/java/dev/daniza/draflix/DraflixActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.daniza.draflix.ui.theme.DraflixTheme
 
-class MainActivity : ComponentActivity() {
+class DraflixActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/dev/daniza/draflix/DraflixApp.kt
@@ -0,0 +1,8 @@
+package dev.daniza.draflix
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DraflixApp : Application() {
+}
