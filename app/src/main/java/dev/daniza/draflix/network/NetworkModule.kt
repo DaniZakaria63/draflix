c26034bfefb5518dcdab68f4e25adafc98c0b838
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-08-23 20:38:09 +0700

    feat(init): init network instance

---
+++ app/src/main/java/dev/daniza/draflix/network/NetworkModule.kt
@@ -0,0 +1,17 @@
+package dev.daniza.draflix.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+    @Singleton
+    @Provides
+    fun provideOMDBService(): OMDBService {
+        return OMDBService.create()
+    }
+}
