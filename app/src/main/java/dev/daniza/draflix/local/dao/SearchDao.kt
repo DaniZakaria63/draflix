bad6c168670d304594a4c6e0dafd9cdb5e7fb806
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-08-24 22:15:06 +0700

    feat(configtest): fix the search param

--- app/src/main/java/dev/daniza/draflix/local/dao/SearchDao.kt
+++ app/src/main/java/dev/daniza/draflix/local/dao/SearchDao.kt
@@ -12,7 +12,7 @@ interface SearchDao {
     @Query("SELECT * FROM search")
     fun getAll(): List<SearchEntity>
 
-    @Query("SELECT * FROM search WHERE type = :type OR title LIKE '%' || :title || '%'")
+    @Query("SELECT * FROM search WHERE type = :type OR title LIKE :title")
     fun getSearchByQuery(type: String = "", title: String = ""): Flow<List<SearchEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
