95b5bf484c2147f403d8b7d4c6e1f07860e5703d
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-08-27 01:18:09 +0700

    feat(beautify): cleaning comment

--- app/src/main/java/dev/daniza/draflix/ui/screen/list/HomeListScreen.kt
+++ app/src/main/java/dev/daniza/draflix/ui/screen/list/HomeListScreen.kt
@@ -250,48 +250,6 @@ fun SearchTextField(
             .fillMaxWidth()
             .padding(vertical = 12.dp)
     )
-    /*
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .then(modifier)
-            .height(42.dp)
-            .fillMaxWidth()
-            .shadow(elevation = 4.dp, shape = RoundedCornerShape(8.dp))
-            .background(
-                color = MaterialTheme.colorScheme.surfaceVariant,
-                shape = RoundedCornerShape(8.dp)
-            ),
-    ) {
-        BasicTextField(
-            value = value,
-            onValueChange = onValueChange,
-            textStyle = TextStyle(
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold
-            ),
-            decorationBox = { innerTextField ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "search.icon",
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                        modifier = Modifier
-                            .padding(horizontal = 12.dp)
-                            .size(30.dp)
-                    )
-                    innerTextField()
-                }
-            },
-            singleLine = true,
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp, horizontal = 12.dp)
-        )
-    }*/
 }
 
 @Composable
